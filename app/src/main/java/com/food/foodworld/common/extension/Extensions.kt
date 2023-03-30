59a81e3b284d7ce7b78ed26f888eb62b0eceac65
Yunus Erbek <yunus_erbek@windowslive.com>
2023-03-30 17:45:10 +0300

    first commit

---
+++ app/src/main/java/com/food/foodworld/common/extension/Extensions.kt
@@ -0,0 +1,28 @@
+package com.example.gameinformation.common.extension
+
+import android.content.Context
+import android.graphics.drawable.Drawable
+import android.widget.ImageView
+import androidx.datastore.preferences.preferencesDataStore
+import androidx.swiperefreshlayout.widget.CircularProgressDrawable
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.food.foodworld.R
+
+fun ImageView.glideImage(url: String?) {
+    Glide.with(this.context).load(url)
+        .diskCacheStrategy(DiskCacheStrategy.DATA)
+        .placeholder(this.context.circularProgressDrawable()).error(R.drawable.exclamation).into(this)
+
+
+}
+fun Context.circularProgressDrawable(): Drawable {
+    return CircularProgressDrawable(this).apply {
+        strokeWidth = 5f
+        centerRadius = 30f
+        start()
+    }
+}
+fun String.titleCaseFirstChar() = replaceFirstChar(Char::titlecase)
+
+val Context.dataStore by preferencesDataStore("last_searched")
